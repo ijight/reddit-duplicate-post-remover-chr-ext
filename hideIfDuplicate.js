ec8e2baafdc01508dbfda44c8fe89ae78d3971b8
knightimecrunch <ianjarrattknight@gmail.com>
2021-07-28 22:39:28 -0500

    removed unecessary files

--- hideIfDuplicate.js
+++ hideIfDuplicate.js
@@ -1,28 +1,36 @@
-window.scrollTo(0,document.body.scrollHeight);
-console.log(removedPostCount);
-document.getElementById("duplicateTestButtonText").innerText = removedPostCount;
-var elements = document.getElementsByClassName("rpBJOHq2PR60pnwJlUyP0")[0];
-console.log(elements);
-var postsList = elements.childNodes;
-for(var i=1;i<postsList.length;i++) {
+var existingImageList = [];
+var removedPostCount = 0;
+var body = document.getElementsByTagName('body');
 
-    var image_container = postsList[i];
-    console.log(postsList[i]);
-    if(typeof postsList[i].getElementsByClassName('media-element')[0] !== 'undefined') {
-        var image = postsList[i].getElementsByClassName('media-element')[0].src;
-    } else {
-        image = "something-else";
-    }
 
-    if (!image_container.classList.contains("post-removal-script-ignore")){
-        if(existingImageList.includes(image)) {
-            image_container.style.display = "none";
-            removedPostCount++;
-            image_container.classList.add("post-removal-script-ignore");
 
+function hide() {
+   
+    var elements = document.getElementsByClassName("rpBJOHq2PR60pnwJlUyP0")[0];
+    var postsList = elements.childNodes;
+    for (var i = 1; i < postsList.length; i++) {
+
+        var image_container = postsList[i];
+        console.log(postsList[i]);
+        if (typeof postsList[i].getElementsByClassName('media-element')[0] !== 'undefined') {
+            var image = postsList[i].getElementsByClassName('media-element')[0].src;
         } else {
-            existingImageList.push(image);
-            image_container.classList.add("post-removal-script-ignore");
+            image = "something-else";
+        }
+
+        if (!image_container.classList.contains("post-removal-script-ignore")) {
+            if (existingImageList.includes(image)) {
+                image_container.style.display = "none";
+                removedPostCount++;
+                image_container.classList.add("post-removal-script-ignore");
+
+            } else {
+                existingImageList.push(image);
+                image_container.classList.add("post-removal-script-ignore");
+            }
         }
     }
+    console.log("I have removed " + removedPostCount + " posts.");
 }
+
+document.getElementsByClassName("_1vyLCp-v-tE5QvZovwrASa")[0].addEventListener("click", hide);
